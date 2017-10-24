7b9f0ac0f9c1721380db4da11c1669ebe0aec86d
jeanpantoja <je.cspantoja@gmail.com>
2017-10-24 17:08:40 -0200

    modifying tests

--- src/test.py
+++ src/test.py
@@ -17,45 +17,45 @@ class TestProfile( unittest.TestCase ):
         profile = core.Profile()
         call = core.Call( core.Call.Type.LONG_DISTANCE,
                           core.Call.DestinyType.MOBILE,
-                          1 )
+                          2 )
 
         profile.add_call( call )
         profile.add_call( call )
-        self.assertEqual( profile.get_long_distance_mobile_call_usage(), 2 )
+        self.assertEqual( profile.get_long_distance_mobile_call_usage(), 4 )
 
     def test_adding_local_landline_call( self ):
         profile = core.Profile()
         call = core.Call( core.Call.Type.LOCAL,
                           core.Call.DestinyType.LANDLINE,
-                          1 )
+                          3 )
 
         profile.add_call( call )
         profile.add_call( call )
-        self.assertEqual( profile.get_local_landline_call_usage(), 2 )
+        self.assertEqual( profile.get_local_landline_call_usage(), 6 )
 
     def test_adding_local_mobile_call( self ):
         profile = core.Profile()
         call = core.Call( core.Call.Type.LOCAL,
                           core.Call.DestinyType.MOBILE,
-                          1 )
+                           4)
 
         profile.add_call( call )
         profile.add_call( call )
-        self.assertEqual( profile.get_local_mobile_call_usage(), 2 )
+        self.assertEqual( profile.get_local_mobile_call_usage(), 8 )
 
     def test_adding_SMS( self ):
         profile = core.Profile()
 
         profile.add_SMS( 1 )
-        profile.add_SMS( 1 )
-        self.assertEqual( profile.get_SMS_usage(), 2 )
+        profile.add_SMS( 2 )
+        self.assertEqual( profile.get_SMS_usage(), 3 )
 
     def test_adding_internet( self ):
         profile = core.Profile()
 
         profile.add_internet( 1 )
-        profile.add_internet( 1 )
-        self.assertEqual( profile.get_internet_usage(), 2 )
+        profile.add_internet( 2 )
+        self.assertEqual( profile.get_internet_usage(), 3 )
 
 if __name__ == "__main__":
     unittest.main()
