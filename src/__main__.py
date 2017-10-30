9f4456a65a6499f0693acd496d30edfabd8d7d4a
jeanpantoja <je.cspantoja@gmail.com>
2017-10-30 18:36:36 -0200

    cleaning to start ruby implementation :P

--- src/__main__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-
-import account_profile.cli as cli
-
-program = cli.Program()
-program.run()
-
--- src/test.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from test.core import *
-from test.parser import *
-import unittest
-
-if __name__ == "__main__":
-    unittest.main()
