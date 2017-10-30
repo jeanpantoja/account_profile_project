17571ca989ddf6f8b50d0f2490e5a4652808604b
jeanpantoja <je.cspantoja@gmail.com>
2017-10-30 19:16:10 -0200

    organizing code

--- src/main/AccountProfiler/Parser/InternetService.rb
+++ src/main/AccountProfiler/Parser/InternetService.rb
@@ -4,7 +4,9 @@ module AccountProfiler
     module Parser
         class InternetService < Service
             def initialize()
-                super( /(TIM\s+\w+\s+Fast)|(BlackBerry\s+Professional\s+-\s+MB)/i )
+                super(
+                    /(TIM\s+\w+\s+Fast)|(BlackBerry\s+Professional\s+-\s+MB)/i
+                )
             end
         end
     end
